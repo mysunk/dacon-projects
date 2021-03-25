bc1cce4e6e4f258dc1177abfce7dd675e4bee632
Myungsun Kim <rlaaudtjs@gist.ac.kr>
2021-03-25 15:28:21 +0900

    update readme and refactor projects

--- PV-forecasting/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for method in dnn rf extra
-do
-  arg1="--save_file=210106/"$method""_""
-  arg3="--method=$method"
-  python tune-main.py "$arg1" "$arg3" --max_evals=1000 # normalize:True in defult
-done
